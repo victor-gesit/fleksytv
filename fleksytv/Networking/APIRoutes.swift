82d81787703616454d0381dace4b05c1169f14ff
Victor Idongesit <victoridongesit@gmail.com>
2022-03-29 16:44:48 +0100

    add models and api configuration routes

--- fleksytv/Networking/APIRoutes.swift
+++ fleksytv/Networking/APIRoutes.swift
@@ -11,18 +11,22 @@ enum APIRoutes {
     case topRatedTVShows
     case tvShowDetail(String)
     case similarTVShows(String)
+    case genres
+    case configuration
 }
 
 extension APIRoutes: CustomStringConvertible {
     var description: String {
         switch self {
-        case .topRatedTVShows: return Constants.BaseURL + "top_rated"
-        case .tvShowDetail(let tvId): return Constants.BaseURL + tvId
-        case .similarTVShows(let tvId): return Constants.BaseURL + tvId + "/similar"
+        case .topRatedTVShows: return Constants.BaseURL + "tv/top_rated"
+        case .tvShowDetail(let tvId): return Constants.BaseURL + "/tv/\(tvId)"
+        case .similarTVShows(let tvId): return Constants.BaseURL + tvId + "/tv/similar"
+        case .genres: return Constants.BaseURL + "genre/movie/list"
+        case .configuration: return Constants.BaseURL + "configuration"
         }
     }
     
     struct Constants {
-        static let BaseURL = "https://api.themoviedb.org/3/tv/"
+        static let BaseURL = "https://api.themoviedb.org/3/"
     }
 }
---
+++ fleksytv/Networking/Models/TVShow.swift
@@ -0,0 +1,14 @@
+//
+//  TVShow.swift
+//  fleksytv
+//
+//  Created by Victor Idongesit on 29/03/2022.
+//
+
+import Foundation
+
+struct TVShow: Codable {
+    var imageURL: String
+    var title: String
+    var overview: String
+}
