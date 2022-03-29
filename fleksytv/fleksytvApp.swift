2deb1ced4a78046a6804812062f9995c94066a62
Victor Idongesit <victoridongesit@gmail.com>
2022-03-29 11:33:46 +0100

    Initial Commit

---
+++ fleksytv/fleksytvApp.swift
@@ -0,0 +1,17 @@
+//
+//  fleksytvApp.swift
+//  fleksytv
+//
+//  Created by Victor Idongesit on 29/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct fleksytvApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
