42c5a9460f3667f4e58d381fa0ee9fdd31448e95
Victor Idongesit <victoridongesit@gmail.com>
2022-03-29 18:13:37 +0100

    create top-rated-shows response model

---
+++ fleksytv/Networking/Models/Data Models/TVShow.swift
@@ -0,0 +1,16 @@
+//
+//  TVShow.swift
+//  fleksytv
+//
+//  Created by Victor Idongesit on 29/03/2022.
+//
+
+import Foundation
+
+struct TVShow: Codable {
+    var posterPath: String?
+    var name: String?
+    var overview: String?
+    var voteAverage: Float?
+    var firstAirDate: String?
+}
