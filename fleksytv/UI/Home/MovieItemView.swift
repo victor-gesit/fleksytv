521e243ddb37051f0c6f3108f902dcad5c5b6871
Victor Idongesit <victoridongesit@gmail.com>
2022-03-30 14:00:28 +0100

    create movies view, and movies item view

---
+++ fleksytv/UI/Home/MovieItemView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieItemView.swift
+//  fleksytv
+//
+//  Created by Victor Idongesit on 30/03/2022.
+//
+
+import SwiftUI
+
+struct MovieItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieItemView()
+    }
+}
---
+++ fleksytv/UI/Home/MoviesView.swift
@@ -0,0 +1,20 @@
+//
+//  MoviesView.swift
+//  fleksytv
+//
+//  Created by Victor Idongesit on 30/03/2022.
+//
+
+import SwiftUI
+
+struct MoviesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MoviesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MoviesView()
+    }
+}
