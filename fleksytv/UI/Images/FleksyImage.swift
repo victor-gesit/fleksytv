3521604fddcb975771242e9b68aa6e1914fcf41a
Victor Idongesit <victoridongesit@gmail.com>
2022-03-30 14:13:18 +0100

    setup custom image class and sample images

---
+++ fleksytv/UI/Images/FleksyImage.swift
@@ -0,0 +1,21 @@
+//
+//  FleksyImage.swift
+//  fleksytv
+//
+//  Created by Victor Idongesit on 30/03/2022.
+//
+
+import Foundation
+import SwiftUI
+
+enum FleksyImage: String {
+    case star
+    case logo
+    case testImageCover
+}
+
+extension Image {
+    static func imageFrom(_ fleksyImage: FleksyImage) -> Image {
+        return Image(fleksyImage.rawValue)
+    }
+}
